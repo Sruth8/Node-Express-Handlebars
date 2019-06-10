ec714abce1e1b99da60727ecc6b2e5fba4ab08f7
Shelisha Rutherford <starshipazul@yahoo.com>
2019-06-10 18:32:44 -0400

    almost

--- config/orm.js
+++ config/orm.js
@@ -11,7 +11,8 @@ var orm = {
 
 // this will update when the burger is eaten 
     update: function(tableInput, condition, callback) {
-        connection.query("UPDATE " + tableInput + "SET devoured=true WHERE id= " + condition + ";", function(err,result){
+        console.log(tableInput, condition, callback)
+        connection.query("UPDATE " + tableInput + " SET devoured=true WHERE id=" + condition + ";", function(err,result){
             if (err)throw err;
             callback(result);
         })
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,13 @@ var burger = require('../models/burger.js'); // referance to burger.js file
 
 
 //set up the router here
-
+router.post("/burgers/update/:id", function(req,res){
+    console.log(req.params);
+    burger.update(req.params.id, function(result){
+        console.log(result);
+        res.redirect("/");
+    });
+});
 router.get("/", function(req,res){
     burger.all(function(burgerInfo){ //passing burgerInfo into the function
         console.log(burgerInfo);
@@ -16,12 +22,7 @@ router.get("/", function(req,res){
     
 })
 
-router.put("/burgers/update", function(req,res){
-    burger.update(req.body.burgerInfo, function(result){
-        console.log(result);
-        res.redirect("/");
-    });
-});
+
 //this will post the burger that was created by the user
 router.post("/burgers/create", function(req,res){
     burger.create(req.body.burger_name, function(result){
